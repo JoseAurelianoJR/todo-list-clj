b1f7ac6319b9fba0045fe7313762f62c7ccc8f7b
Jose Aureliano <junior.rjsistemas@gmail.com>
2017-04-05 21:40:37 -0300

    adding webserver

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,7 +1,10 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "Web server."
+  [port]
+  (fn [request] {:status 200
+                 :body "<p>Test app</p>"
+                 :headers {}})
+  {:port (Integer. port)})
