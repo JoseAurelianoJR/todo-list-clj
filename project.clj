9ba8ab7d596f3eb07f1dc975dcc8de17d3289b50
Jose Aureliano <junior.rjsistemas@gmail.com>
2017-04-05 21:28:43 -0300

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject todo-list "0.1.0-SNAPSHOT"
+  :description "Todo list web app with Ring and Compojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
