adf8038a9da5c124943256dff1bffc1037bb8005
Jose Aureliano <junior.rjsistemas@gmail.com>
2017-04-05 21:36:45 -0300

    adding ring and include namespace on core file

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject todo-list "0.1.0-SNAPSHOT"
   :description "Todo list web app with Ring and Compojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/JoseAurelianoJR/todo-list-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [ring "1.6.0-RC1"]]
+  :main todo-list.core)
