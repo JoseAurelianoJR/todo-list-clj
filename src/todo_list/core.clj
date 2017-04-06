9699f24d77bef3c75c86f582e1f00da201412b58
Jose Aureliano <junior.rjsistemas@gmail.com>
2017-04-05 22:21:55 -0300

    refactor home funciont

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,11 +1,15 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn home
+  "Application home page",
+  [request]
+  {:status 200
+   :body "<h2>Todo List App</h2>"
+   :headers {}})
+
 (defn -main
   "Web server."
   [port]
-  (jetty/run-jetty
-    (fn [request] {:status 200
-                   :body "<p>Test app</p>"
-                   :headers {}})
+  (jetty/run-jetty home
     {:port (Integer. port)}))
