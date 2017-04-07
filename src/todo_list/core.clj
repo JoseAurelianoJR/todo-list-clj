c16f4c9209106ea2bff8343c7d95d9f00872cf61
Jose Aureliano <junior.rjsistemas@gmail.com>
2017-04-06 21:42:55 -0300

    configuring middleware for auto refresh change with stop and start server action

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,12 +1,13 @@
 (ns todo-list.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+    [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn home
   "Application home page",
   [request]
   (if (= "/" (:uri request))
     {:status 200
-     :body "<h2>Todo List App</h2>"
+     :body "<h2>Todo List Application</h2>"
      :headers {}}
 
      {:status 404
@@ -18,3 +19,9 @@
   [port]
   (jetty/run-jetty home
     {:port (Integer. port)}))
+
+(defn -dev-main
+  "ring and jetty live reload"
+  [port]
+  (jetty/run-jetty (wrap-reload #'home)
+  {:port (Integer. port)}))
